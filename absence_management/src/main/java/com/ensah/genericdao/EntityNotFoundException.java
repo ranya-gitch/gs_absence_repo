f8b217c251a47179adfd5c1e2e01456d359e198e
Tarik BOUDAA <tarikboudaa@yahoo.fr>
2021-06-09 16:52:34 +0100

    Correction du problème Lazy Exception et ajout de l'accès aux informations de l'utilisateur

---
+++ absence_management/src/main/java/com/ensah/genericdao/EntityNotFoundException.java
@@ -0,0 +1,28 @@
+package com.ensah.genericdao;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	public EntityNotFoundException() {
+		super();
+	}
+
+	public EntityNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public EntityNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+
+	public EntityNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+
+
+}
